5b54d492f10819f32818d1590aa5df6fbf1d0af7
Badlord525 <badlord525@gmail.com>
2023-01-26 14:20:13 +0200

    added design

--- header/app/js/main.js
+++ header/app/js/main.js
@@ -8,5 +8,15 @@ $(function(){
         autoplaySpeed: 2000
     });
 
-    var mixer = mixitup('.products__items');
+    var containerEl1 = document.querySelector('[data-ref="products-categories"]');
+    var containerEl2 = document.querySelector('[data-ref="design-categories"]');
+ 
+    var config = {
+    controls: {
+      scope: 'local'
+        }
+    };
+ 
+    var mixer1 = mixitup(containerEl1, config);
+    var mixer2 = mixitup(containerEl2, config);
 });
